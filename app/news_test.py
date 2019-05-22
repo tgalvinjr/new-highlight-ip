b57b50ae2959fb61645df14b9064982b089d7d62
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-22 15:57:51 +0300

    create test for the News Class

--- app/news_test.py
+++ app/news_test.py
@@ -1,1 +1,14 @@
 import unittest 
+from models import news
+News = news.News
+
+class NewsTest (unittest.TestCase):
+
+    def setUp(self):
+        self.new_news = News(id,name,author,title,description,urlToImage,publishedAt,)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+if __name__ == '__main__':
+    unittest.main()
