1bcc59c61c9d250af87c33475787b913060ecb4e
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-22 16:26:59 +0300

    create News Class to pass test

---
+++ app/models/news.py
@@ -0,0 +1,9 @@
+class News:
+    def __init__(self, id, name, author,title, description, urlToImage, publishedAt):
+        self.id = id
+        self.name= name
+        self.author = author
+        self.title= title
+        self.description = description
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
--- app/news_test.py
+++ app/news_test.py
@@ -1,11 +1,11 @@
 import unittest 
-from models import news
+from .models import news
 News = news.News
 
 class NewsTest (unittest.TestCase):
 
     def setUp(self):
-        self.new_news = News(id,name,author,title,description,urlToImage,publishedAt,)
+        self.new_news = News("id",'name','author','title','description','urlToImage','publishedAt')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
