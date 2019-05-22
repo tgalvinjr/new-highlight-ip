a273279f11ee2b7a8058f9a05a9218d4e9d2ce94
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-22 12:44:23 +0300

    add news_article.html and define the route

--- app/views.py
+++ app/views.py
@@ -2,10 +2,14 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/')
+@app.route('/') #homepage route
 def index():
     message = 'Welcome to your favorite News Channel'
-    return render_template('index.html',message=message)
+    return render_template('index.html', message=message)
 
 
 
+@app.route('/news_article') #news article route
+def news():
+    
+    return render_template('news_article.html')
